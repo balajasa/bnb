91667f8953713f24da43c5fb51886998396427b7
miya_xu <miya_xu@mail.chungyo.net>
2023-04-28 14:16:55 +0800

    test

--- pages/index/Counter.vue
+++ pages/index/Counter.vue
@@ -1,8 +1,11 @@
 <template>
-  <button type="button" @click="state.count++">Counter {{ state.count }}</button>
+  <button type="button" @click="counterStore.increment()">Counter {{ data }}</button>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
-const state = reactive({ count: 0 })
+import { computed } from 'vue'
+import { useCounter } from '@/stores/useCounter'
+
+const counterStore = useCounter()
+const data = computed(() => counterStore.count)
 </script>
